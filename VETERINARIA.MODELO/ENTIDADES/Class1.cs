19e0bf740943b49c7bcc0ce0f06985ac20bccf2e
asilvestri <asilvestri@seguridadvial.gob.ar>
2023-02-07 01:55:40 -0300

    ADD: Se agrega el sitio en ASP con el template integrado.-

---
+++ VETERINARIA.MODELO/ENTIDADES/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VETERINARIA.MODELO.ENTIDADES
+{
+    class Class1
+    {
+    }
+}
